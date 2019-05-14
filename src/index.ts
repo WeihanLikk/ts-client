9138d4dbcc9c55fea14ae75973e58e0d5b7f78b2
lincandong <571611998@qq.com>
2019-05-14 20:03:26 +0800

    t

--- src/index.ts
+++ src/index.ts
@@ -136,34 +136,6 @@ class WebSocket {
     }
 }
 
-manager.load([
-    "export/Building_Auto Service",
-    "export/Building_Bakery",
-    "export/Building_Bar",
-    "export/Building_Books Shop",
-    "export/Building_Chicken Shop",
-    "export/Building_Clothing",
-    "export/Building_Coffee Shop",
-    "export/Building_Drug Store",
-    "export/Building_Factory",
-    "export/Building_Fast Food",
-    "export/Building_Fruits  Shop",
-    "export/Building_Gas Station",
-    "export/Building_Gift Shop",
-    "export/Building_House_01_color01",
-    "export/Building_House_02_color01",
-    "export/Building_House_03_color01",
-    "export/Building_House_04_color01",
-    "export/Building_Music Store",
-    "export/Building_Pizza",
-    "export/Building_Residential_color01",
-    "export/Building_Restaurant",
-    "export/Building_Shoes Shop",
-    "export/Building Sky_big_color01",
-    "export/Building Sky_small_color01",
-    "export/Building_Stadium",
-    "export/Building_Super Market"
-]).then(() => {
+
     const ws = new WebSocket("localhost", 8899)
-})
 
