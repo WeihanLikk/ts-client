c583943bc9604d56934b42844b05509cddd9d7da
lincandong <571611998@qq.com>
2019-05-17 13:59:01 +0800

    good

--- dist/basemap/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./basemap";
-export * from "./buildingItem";
-export * from "./def";
-export * from "./geometry";
-export * from "./roadItem";
--- dist/wasp/assets/json.d.ts
+++
@@ -1,9 +0,0 @@
-import { Asset } from "./asset";
-export declare class JsonAsset extends Asset<{
-    [key: string]: any;
-}> {
-    constructor(path: string);
-    load(): Promise<{
-        [key: string]: any;
-    }>;
-}
